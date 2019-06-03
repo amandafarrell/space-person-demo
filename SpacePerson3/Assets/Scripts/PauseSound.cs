5a6e387728990e2dc348972eeb9b32ad506a245c
Amanda Farrell <bluepandadev@gmail.com>
2019-06-02 22:12:46 -0600

    Music can be stopped, but not restarted

---
+++ SpacePerson3/Assets/Scripts/PauseSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSound : MonoBehaviour
+{
+   
+    public void Pause()
+    {
+        AudioListener.pause = true;        
+    }
+}
